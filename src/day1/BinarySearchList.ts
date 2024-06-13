772a9af4cf38210122a99baf29927a00783e263a
shivkumarojha <ojhashivkumar88@gmail.com>
2024-06-13 15:36:45 +0530

    Binary search is done

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,23 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+    let lo = 0
+    let hi = haystack.length
+    do {
+        const m = Math.floor(lo + (hi - lo) / 2)
+        const v = haystack[m]
+
+        if (v === needle) {
+            return true
+        } else if (v > needle) {
+            hi = m
+        } else {
+            lo = m + 1
+        }
+    } while (lo < hi)
+
+    return false
 }
+
+const array = [1, 3, 4, 5, 6, 7, 8]
+const result = bs_list(array, 1)
+console.log(result)
--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,11 +1,12 @@
 export default function linear_search(haystack: number[], needle: number): boolean {
     for (let i = 0; i < haystack.length; i++) {
-        if(haystack[i] == needle) {
+        if (haystack[i] == needle) {
             return true
         }
     }
     return false
 }
 
-let haystack = [1,4,5,2,5,5]
-let answer = linear_search(haystack, 3)
+let haystack = [1, 4, 5, 2, 5, 5]
+let answer = linear_search(haystack, 4)
+console.log(answer)
